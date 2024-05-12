66777432fd51374ae032b6b57aed2e796b9ec056
nomanmujahid1144 <nomanmujahid.cs@gmail.com>
2024-05-12 16:24:13 +0500

    first commit

--- src/updateLeaderboardWithPublicKey.tsx
+++ src/updateLeaderboardWithPublicKey.tsx
@@ -3,7 +3,8 @@ import { useWallet } from "@solana/wallet-adapter-react";
 import axios from 'axios';
 
 let wallet_address = "";
-const baseURL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:9001';
+// const baseURL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:9001';
+const baseURL = 'https://ep-damp-breeze-a282mcll-pooler.eu-central-1.aws.neon.tech/';
 // const baseURL = 'http://localhost:9001';
 
 export const updateLeaderboardWithPublicKey = async (score: number) => {
